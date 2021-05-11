1ef6ac91326918a7f010e17b7ce89068e0f32249
Rico Guinanao <83864808+coricss@users.noreply.github.com>
2021-05-11 20:11:08 +0800

    Add files via upload

---
+++ LogInForm(Guinanao-Agapito-Cornita)/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace LogInForm_Guinanao_Agapito_Cornita_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnexit = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.eye = new System.Windows.Forms.PictureBox();
+            this.logo = new System.Windows.Forms.PictureBox();
+            this.picPass = new System.Windows.Forms.PictureBox();
+            this.picUser = new System.Windows.Forms.PictureBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.eye)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPass)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picUser)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.SpringGreen;
+            this.panel3.Controls.Add(this.btnexit);
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(447, 29);
+            this.panel3.TabIndex = 12;
+            // 
+            // btnexit
+            // 
+            this.btnexit.BackColor = System.Drawing.Color.SpringGreen;
+            this.btnexit.BackgroundImage = global::LogInForm_Guinanao_Agapito_Cornita_.Properties.Resources.exit2;
+            this.btnexit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnexit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnexit.FlatAppearance.BorderColor = System.Drawing.Color.SpringGreen;
+            this.btnexit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnexit.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnexit.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnexit.Location = new System.Drawing.Point(380, 1);
+            this.btnexit.Name = "btnexit";
+            this.btnexit.Size = new System.Drawing.Size(30, 28);
+            this.btnexit.TabIndex = 4;
+            this.btnexit.Tag = "";
+            this.toolTip1.SetToolTip(this.btnexit, "Click to exit.");
+            this.btnexit.UseVisualStyleBackColor = false;
+            this.btnexit.Click += new System.EventHandler(this.btnexit_Click);
+            this.btnexit.MouseLeave += new System.EventHandler(this.btnexit_MouseLeave);
+            this.btnexit.MouseHover += new System.EventHandler(this.btnexit_MouseHover);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Window;
+            this.panel2.Location = new System.Drawing.Point(55, 307);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(302, 1);
+            this.panel2.TabIndex = 13;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPassword.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.ForeColor = System.Drawing.Color.Gray;
+            this.txtPassword.Location = new System.Drawing.Point(107, 276);
+            this.txtPassword.MaxLength = 6;
+            this.txtPassword.Multiline = true;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(204, 32);
+            this.txtPassword.TabIndex = 16;
+            this.txtPassword.Text = "Password";
+            this.toolTip1.SetToolTip(this.txtPassword, "Enter your password.");
+            this.txtPassword.UseSystemPasswordChar = true;
+            this.txtPassword.Click += new System.EventHandler(this.txtPassword_Click);
+            this.txtPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Window;
+            this.panel1.Location = new System.Drawing.Point(56, 246);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(302, 1);
+            this.panel1.TabIndex = 14;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.txtUsername.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUsername.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername.ForeColor = System.Drawing.Color.Gray;
+            this.txtUsername.Location = new System.Drawing.Point(105, 214);
+            this.txtUsername.MaxLength = 12;
+            this.txtUsername.Multiline = true;
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(250, 33);
+            this.txtUsername.TabIndex = 15;
+            this.txtUsername.Text = "Username";
+            this.toolTip1.SetToolTip(this.txtUsername, "Enter your username.");
+            this.txtUsername.Click += new System.EventHandler(this.txtUsername_Click);
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.BackColor = System.Drawing.Color.SpringGreen;
+            this.btnlogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnlogin.FlatAppearance.BorderColor = System.Drawing.Color.SpringGreen;
+            this.btnlogin.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnlogin.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnlogin.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnlogin.Location = new System.Drawing.Point(56, 342);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(302, 50);
+            this.btnlogin.TabIndex = 17;
+            this.btnlogin.Tag = "";
+            this.btnlogin.Text = "LOG IN";
+            this.toolTip1.SetToolTip(this.btnlogin, "Click to log in.");
+            this.btnlogin.UseVisualStyleBackColor = false;
+            this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.toolTip1.ForeColor = System.Drawing.SystemColors.Info;
+            this.toolTip1.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            // 
+            // eye
+            // 
+            this.eye.BackgroundImage = global::LogInForm_Guinanao_Agapito_Cornita_.Properties.Resources.eye1;
+            this.eye.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.eye.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.eye.Enabled = false;
+            this.eye.Location = new System.Drawing.Point(317, 264);
+            this.eye.Name = "eye";
+            this.eye.Size = new System.Drawing.Size(37, 37);
+            this.eye.TabIndex = 21;
+            this.eye.TabStop = false;
+            this.toolTip1.SetToolTip(this.eye, "Show password.");
+            this.eye.Click += new System.EventHandler(this.eye_Click);
+            // 
+            // logo
+            // 
+            this.logo.BackgroundImage = global::LogInForm_Guinanao_Agapito_Cornita_.Properties.Resources.logo;
+            this.logo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.logo.Location = new System.Drawing.Point(126, 44);
+            this.logo.Name = "logo";
+            this.logo.Size = new System.Drawing.Size(163, 146);
+            this.logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.logo.TabIndex = 20;
+            this.logo.TabStop = false;
+            // 
+            // picPass
+            // 
+            this.picPass.BackgroundImage = global::LogInForm_Guinanao_Agapito_Cornita_.Properties.Resources.password1;
+            this.picPass.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picPass.Location = new System.Drawing.Point(52, 259);
+            this.picPass.Name = "picPass";
+            this.picPass.Size = new System.Drawing.Size(57, 46);
+            this.picPass.TabIndex = 19;
+            this.picPass.TabStop = false;
+            // 
+            // picUser
+            // 
+            this.picUser.BackgroundImage = global::LogInForm_Guinanao_Agapito_Cornita_.Properties.Resources.user1;
+            this.picUser.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picUser.Location = new System.Drawing.Point(56, 198);
+            this.picUser.Name = "picUser";
+            this.picUser.Size = new System.Drawing.Size(45, 42);
+            this.picUser.TabIndex = 18;
+            this.picUser.TabStop = false;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Silver;
+            this.panel4.ForeColor = System.Drawing.Color.Silver;
+            this.panel4.Location = new System.Drawing.Point(225, 436);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(133, 1);
+            this.panel4.TabIndex = 14;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Silver;
+            this.panel5.ForeColor = System.Drawing.Color.Silver;
+            this.panel5.Location = new System.Drawing.Point(55, 436);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(128, 1);
+            this.panel5.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Unicode MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Silver;
+            this.label1.Location = new System.Drawing.Point(192, 424);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 21);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "or";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.LinkColor = System.Drawing.Color.SpringGreen;
+            this.linkLabel1.Location = new System.Drawing.Point(179, 471);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(53, 16);
+            this.linkLabel1.TabIndex = 23;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "SIGN UP";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.ClientSize = new System.Drawing.Size(413, 522);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.btnlogin);
+            this.Controls.Add(this.eye);
+            this.Controls.Add(this.logo);
+            this.Controls.Add(this.picPass);
+            this.Controls.Add(this.picUser);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.eye)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPass)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picUser)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnexit;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.PictureBox eye;
+        private System.Windows.Forms.PictureBox logo;
+        private System.Windows.Forms.PictureBox picPass;
+        private System.Windows.Forms.PictureBox picUser;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ LogInForm(Guinanao-Agapito-Cornita)/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Synthesis;
+
+
+namespace LogInForm_Guinanao_Agapito_Cornita_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            reader.Dispose();
+            reader = new SpeechSynthesizer();
+            reader.SelectVoiceByHints(VoiceGender.Female);
+            reader.SpeakAsync("Please log in.");
+        }
+        SpeechSynthesizer reader = new SpeechSynthesizer();
+        public void Clr()
+        {
+            txtUsername.Text = "Username";
+            txtUsername.ForeColor = Color.Gray;
+            txtPassword.Text = "Password";
+            txtPassword.ForeColor = Color.Gray;
+            picPass.BackgroundImage = Properties.Resources.password1;
+            panel2.BackColor = SystemColors.Window;
+            picUser.BackgroundImage = Properties.Resources.user1;
+            panel1.BackColor = SystemColors.Window;
+            txtPassword.UseSystemPasswordChar = true;
+
+        }
+
+        private void txtUsername_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "Username")
+            {
+                txtUsername.Text = "";
+            }
+            txtUsername.ForeColor = SystemColors.Window;
+            picUser.BackgroundImage = Properties.Resources.user2;
+            panel1.BackColor = Color.SpringGreen;
+
+            picPass.BackgroundImage = Properties.Resources.password1;
+            panel2.BackColor = SystemColors.Window;
+
+
+            if (txtPassword.Text == "")
+            {
+                txtPassword.Text = "Password";
+                txtPassword.ForeColor = Color.Gray;
+                txtPassword.UseSystemPasswordChar = true;
+
+            }
+            if (txtPassword.Text == "Password")
+            {
+                eye.Enabled = false;
+            }
+
+
+        }
+
+        private void txtPassword_Click(object sender, EventArgs e)
+        {
+            if (txtPassword.Text == "Password")
+            {
+                txtPassword.Text = "";
+
+
+            }
+
+            txtPassword.ForeColor = SystemColors.Window;
+            picPass.BackgroundImage = Properties.Resources.password2;
+            panel2.BackColor = Color.SpringGreen;
+
+            picUser.BackgroundImage = Properties.Resources.user1;
+            panel1.BackColor = SystemColors.Window;
+            if (txtUsername.Text == "")
+            {
+                txtUsername.Text = "Username";
+                txtUsername.ForeColor = Color.Gray;
+            }
+            if (txtPassword.Text == "Password")
+            {
+                eye.Enabled = false;
+            }
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+            txtPassword.UseSystemPasswordChar = false;
+            eye.Enabled = true;
+        }
+
+        private void eye_Click(object sender, EventArgs e)
+        {
+            if (txtPassword.UseSystemPasswordChar == true)
+            {
+                txtPassword.UseSystemPasswordChar = false;
+                eye.BackgroundImage = Properties.Resources.eye1;
+            }
+            else
+            {
+                txtPassword.UseSystemPasswordChar = true;
+                eye.BackgroundImage = Properties.Resources.eye2;
+            }
+        }
+
+        private void btnexit_MouseHover(object sender, EventArgs e)
+        {
+            btnexit.BackgroundImage = Properties.Resources.exit1;
+        }
+
+        private void btnexit_MouseLeave(object sender, EventArgs e)
+        {
+            btnexit.BackgroundImage = Properties.Resources.exit2;
+        }
+
+        private void btnexit_Click(object sender, EventArgs e)
+        {
+            reader.Dispose();
+            reader = new SpeechSynthesizer();
+            reader.SelectVoiceByHints(VoiceGender.Female);
+            reader.SpeakAsync("Are you really want to exit?");
+            DialogResult dialog = MessageBox.Show("Are you really want to exit?", "Exit",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dialog == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            else if (dialog == DialogResult.No)
+            {
+                Clr();
+                logo.BackgroundImage = Properties.Resources.logo;
+
+            }
+
+        }
+
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+            string[] user = { "rico_", "pauL", "Jeff" };
+            string[] pass = { "rics", "aga", "jeff_" };
+            if (txtUsername.Text == user[0] && txtPassword.Text == pass[0])
+            {
+                logo.BackgroundImage = Properties.Resources.logo2;
+                reader.Dispose();
+                reader = new SpeechSynthesizer();
+                reader.SelectVoiceByHints(VoiceGender.Female);
+                reader.SpeakAsync("You have successfully logged in Rico");
+                MessageBox.Show("You have successfully logged in Rico", "Log In", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+            }
+            else if (txtUsername.Text == user[1] && txtPassword.Text == pass[1])
+            {
+                logo.BackgroundImage = Properties.Resources.logo2;
+                reader.Dispose();
+                reader = new SpeechSynthesizer();
+                reader.SelectVoiceByHints(VoiceGender.Female);
+                reader.SpeakAsync("You have successfully logged in Paul");
+                MessageBox.Show("You have successfully logged in Paul", "Log In", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            else if (txtUsername.Text == user[2] && txtPassword.Text == pass[2])
+            {
+                logo.BackgroundImage = Properties.Resources.logo2;
+                reader.Dispose();
+                reader = new SpeechSynthesizer();
+                reader.SelectVoiceByHints(VoiceGender.Female);
+                reader.SpeakAsync("You have successfully logged in Jeff");
+                MessageBox.Show("You have successfully logged in Jeff", "Log In", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            //else if (txtPassword.MaxLength <6)
+            //{
+            //    reader.Dispose();
+            //    reader = new SpeechSynthesizer();
+            //    reader.SelectVoiceByHints(VoiceGender.Female);
+            //    reader.SpeakAsync("Enter 6 characters or above for password");
+            //    MessageBox.Show("Enter 6 characters or above for password", "Password", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            //}
+            else if (txtUsername.Text != user[0] || txtUsername.Text != user[1] || txtUsername.Text !=
+                user[2] && txtPassword.Text != pass[0] || txtPassword.Text != pass[1] || txtPassword.Text != pass[2])
+            {
+                logo.BackgroundImage = Properties.Resources.logo3;
+                picUser.BackgroundImage = Properties.Resources.user3;
+                picPass.BackgroundImage = Properties.Resources.password3;
+                panel1.BackColor = Color.Red;
+                panel2.BackColor = Color.Red;
+                txtUsername.ForeColor = Color.Red;
+                txtPassword.ForeColor = Color.Red;
+                reader.Dispose();
+                reader = new SpeechSynthesizer();
+                reader.SelectVoiceByHints(VoiceGender.Female);
+                reader.SpeakAsync("Invalid username or password. Please try again");
+                MessageBox.Show("Invalid username or password.\nPlease try again", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Clr();
+                logo.BackgroundImage = Properties.Resources.logo;
+            }
+        }
+
+        
+    }
+}
+
